8ac513bc824363a3a514b2f803e59d35cfdb2680
sayedalif <sayedalif18@gmail.com>
2022-03-01 12:41:20 +0600

    card added

--- js/script.js
+++ js/script.js
@@ -42,5 +42,29 @@ const loadPhoneDetail = (phoneId) => {
 };
 
 const displayPhoneDetail = (phone) => {
-  console.log(phone);
+  const phoneDetail = document.getElementById('phone-detail');
+  const div = document.createElement('div');
+  div.classList.add('card');
+  div.innerHTML = `
+  <div class="row g-0">
+        <div class="col-md-4">
+          <img src="..." class="img-fluid rounded-start" alt="..." />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+  `;
+
+  phoneDetail.appendChild(div);
 };
